c1d3b06e7230b2e6cabefa6ba15994c7293b43f3
Rushikesh Shinde :) <120033565+rushi-commits@users.noreply.github.com>
2023-08-29 12:10:35 +0000

    data types  complited & have chack MDN & tc39.es

--- 01.basics/02_dataTypes.js
+++ 01.basics/02_dataTypes.js
@@ -1,6 +1,22 @@
-// "use strict"; // Treat all JS code as newer version
+"use strict"; // Treat all JS code as newer version
 //  alart(3 + 3); // here we are not using node here not the browser engine
 
 console.log(3 + 3);
-
+// code should be readable
 console.log("Rushi");
+
+let name = "rushi";
+let age = 23;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+//BigInt
+console.log(typeof "abc"); //string =>""
+//boolean => true /false
+//null => standalone value
+//undefined =>
+//symbol = unique
+
+//object
+
+console.log(typeof null);
