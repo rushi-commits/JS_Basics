62307d50075132b449fae122bb93e6580f0a4a21
Rushikesh Shinde :) <120033565+rushi-commits@users.noreply.github.com>
2023-08-29 11:57:32 +0000

    some more changes

---
+++ 01.basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553;
+let accountEmail = "rushi@google.com";
+var accountPassword = "123456";
+accountCity = "pune";
+let accountState;
+accountState = "bjajd";
+// accountId = 2; //can not able to reassigne const
+console.log(accountId);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+/*
+Key point to Remember 
+1. const is not a variable it is a constant
+2. let can be reassigne but can          n    o    t       redeclayer
+3. let is function Scope
+4. Dont use var it creats Jargans
+5. var is block scope and can be assigne declayer and reassigne any time 
+ */
---
+++ 01.basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+// "use strict"; // Treat all JS code as newer version
+//  alart(3 + 3); // here we are not using node here not the browser engine
+
+console.log(3 + 3);
+
+console.log("Rushi");
