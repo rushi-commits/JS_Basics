b914e7304714db6400e7d6ec3dcb6f120a22d8fc
Rushikesh Shinde :) <120033565+rushi-commits@users.noreply.github.com>
2023-09-01 06:36:31 +0000

    date is complited

--- 01.basics/05_string.js
+++ 01.basics/05_string.js
@@ -2,3 +2,6 @@ const name = "Rusikesh";
 const repoCount = 112;
 
 console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+let myName = new String("Rushikesh");
+console.log(myName.__prototype__);
